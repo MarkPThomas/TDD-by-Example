ffc13d10f17508631e6c4199651ad074b1176c0a
MarkPThomas <markums@gmail.com>
2016-11-20 12:04:45 -0800

    Completed Ch. 1
    Yay.

---
+++ 1 - The Money Example/1 - Multi-Currency Money/WyCash/src/WyCash.Core/Dollar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyCash.Core
+{
+    public interface IDollar
+    {
+        int Amount { get; } 
+        void Times(int factor);
+    }
+
+    // Note: Steps of refactoring are preserved and annotated below.
+    // This illustrates the minimum steps needed to make the test pass first.
+    // Then it shows how the class is modified to remove duplication, as well as dependency on test parameters - without breaking the test.
+    public class Dollar : IDollar
+    {
+        // Step 1: Set this constant to = 10;
+        // Step 3: Remove the assignment.
+        public int Amount { get; private set; } 
+
+        public Dollar(int amount)
+        {
+            // Step 1: This is left empty.
+            // Step 2: 
+            Amount = amount;
+        }
+
+        public void Times(int multiplier)
+        {
+            // Step 1: This is left empty.
+            // Step 3: This is changed to to reflect the test setup: 
+            //  Amount = 5*2;
+            // Step 4: 
+            //  Amount = Amount * 2;
+            // Step 5:
+            Amount *= multiplier;
+        }
+    }
+}
---
+++ 1 - The Money Example/1 - Multi-Currency Money/WyCash/src/WyCash.UnitTests/CurrencyTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using WyCash.Core;
+
+namespace WyCash.UnitTests
+{
+    [TestFixture]
+    public class CurrencyTests
+    {
+        [Test]
+        public void testMultiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.Times(2);
+            Assert.AreEqual(10, five.Amount);
+        }
+    }
+}
