ee9eccd3e588f8ffe4b9167f799b929a1d45e515
MarkPThomas <markums@gmail.com>
2016-11-21 00:27:06 -0800

    Completed Ch. 5

---
+++ 1 - The Money Example/5 - Franc-ly Speaking/WyCash/src/WyCash.Core/Dollar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyCash.Core
+{
+    // Note: Steps of refactoring are preserved and annotated below.
+    // This illustrates the minimum steps needed to make the test pass first.
+    // Then it shows how the class is modified to remove duplication, as well as dependency on test parameters - without breaking the test.
+    public class Dollar 
+    {
+        private int _amount;
+
+        public Dollar(int amount)
+        {
+            _amount = amount;
+        }
+
+        public Dollar Times(int multiplier)
+        {
+            return new Dollar(_amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Dollar dollar = (Dollar)obj;
+            return _amount == dollar._amount;
+        }
+    }
+}
---
+++ 1 - The Money Example/5 - Franc-ly Speaking/WyCash/src/WyCash.Core/Frank.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WyCash.Core
+{
+    // Note: Steps of refactoring are preserved and annotated below.
+    // This illustrates the minimum steps needed to make the test pass first.
+    // Then it shows how the class is modified to remove duplication, as well as dependency on test parameters - without breaking the test.
+    public class Franc 
+    {
+        private int _amount;
+
+        public Franc(int amount)
+        {
+            _amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(_amount * multiplier);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Franc dollar = (Franc)obj;
+            return _amount == dollar._amount;
+        }
+    }
+}
