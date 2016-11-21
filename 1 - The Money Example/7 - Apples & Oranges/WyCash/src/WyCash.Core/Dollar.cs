9bcc091ec916693315216574087fb6f9c22d2399
MarkPThomas <markums@gmail.com>
2016-11-21 00:51:59 -0800

    Completed Ch. 7

---
+++ 1 - The Money Example/7 - Apples & Oranges/WyCash/src/WyCash.Core/Dollar.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WyCash.Core
+{
+    // Note: Steps of refactoring are preserved and annotated below.
+    // This illustrates the minimum steps needed to make the test pass first.
+    // Then it shows how the class is modified to remove duplication, as well as dependency on test parameters - without breaking the test.
+    public class Dollar : Money
+    {
+        public Dollar(int amount)
+        {
+            _amount = amount;
+        }
+
+        public Dollar Times(int multiplier)
+        {
+            return new Dollar(_amount * multiplier);
+        }
+    }
+}
---
+++ 1 - The Money Example/7 - Apples & Oranges/WyCash/src/WyCash.Core/Frank.cs
@@ -0,0 +1,19 @@
+﻿
+namespace WyCash.Core
+{
+    // Note: Steps of refactoring are preserved and annotated below.
+    // This illustrates the minimum steps needed to make the test pass first.
+    // Then it shows how the class is modified to remove duplication, as well as dependency on test parameters - without breaking the test.
+    public class Franc : Money
+    { 
+        public Franc(int amount)
+        {
+            _amount = amount;
+        }
+
+        public Franc Times(int multiplier)
+        {
+            return new Franc(_amount * multiplier);
+        }
+    }
+}
