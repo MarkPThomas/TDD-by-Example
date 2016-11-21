ee60487e48940ea84d93a30bf772cb1b56ad4783
MarkPThomas <markums@gmail.com>
2016-11-21 00:47:03 -0800

    Removed ununsed "using" statements.

--- 1 - The Money Example/5 - Franc-ly Speaking/WyCash/src/WyCash.Core/Frank.cs
+++ 1 - The Money Example/5 - Franc-ly Speaking/WyCash/src/WyCash.Core/Frank.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WyCash.Core
+﻿namespace WyCash.Core
 {
     // Note: Steps of refactoring are preserved and annotated below.
     // This illustrates the minimum steps needed to make the test pass first.
